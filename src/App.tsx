20b64643a2e6f26013e9ca3a28758b42c3a44a22
Luka Butskhrikidze <145955971+lukabuts@users.noreply.github.com>
2024-09-14 18:08:27 +0400

    feat: add custom font and necessary SVG icons

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,9 @@
 function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world!</h1>;
+  return (
+    <div className="font-firago">
+      <h1 className="text-3xl font-bold underline">Hello world!</h1>
+    </div>
+  );
 }
 
 export default App;
